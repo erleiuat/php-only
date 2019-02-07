e095d932afbf611836c47b47edc7859225b4a224
Elia Reutlinger <elia.reutlinger@gibmit.ch>
2019-02-07 13:57:23 +0100

    init

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8"> <![end if]-->
+<!--[if !IE]><!--> <html lang="en"> <!--<![end if]-->
+
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Stylessheets -->
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/header.css" />
+    <link rel="stylesheet" href="css/content.css" />
+    <link rel="stylesheet" href="css/footer.css" />
+
+</head>
+<body>
+    <div class="container">
+
+        <div class="header">
+            <div class="header-title">
+                <h1>Onlineshop</h1>
+            </div>
+            <div class="header-content">
+                <div class="header-items">
+                    <ul>
+                        <li><a href="default.asp">Home</a></li>
+                        <li><a href="news.asp">News</a></li>
+                        <li><a href="contact.asp">Contact</a></li>
+                        <li><a href="about.asp">About</a></li>
+                    </ul>
+                </div>
+                <div class="header-icons">
+                    <ul>
+                        <li><a href="default.asp">A</a></li>
+                        <li><a href="news.asp">B</a></li>
+                        <li><a href="contact.asp">C</a></li>
+                        <li><a href="about.asp">D</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="content">
+            <h1>Pagetitle</h1>
+        </div>
+        <div class="footer">
+            <p>
+                Footer-Text
+            </p>
+        </div>
+
+    </div>
+</body>
+</html>
